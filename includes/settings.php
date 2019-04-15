67f87123d620ec39b7233c6d7cfdad6945cab525
Varun <varunsridharan23@gmail.com>
2019-04-15 07:20:54 +0530

    Updated Settings.

--- includes/settings.php
+++ includes/settings.php
@@ -5,6 +5,9 @@
  * Date: 27-11-2018
  * Time: 06:31 PM
  */
+
+use WPO\Container;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	die;
 }
@@ -18,75 +21,19 @@ if ( ! class_exists( 'VSP_Sample_Settings' ) ) {
 		/**
 		 * Add Pages To WPOnion Settings.
 		 *
-		 * @param array $pages
-		 *
-		 * @return array|mixed
+		 * @param array|\WPO\Builder $builder
 		 */
-		public function add_pages( $pages = array() ) {
-			$pages['page1'] = array(
-				'title' => __( 'Page 1' ),
-				'name'  => 'page1',
-				'icon'  => 'dashicons dashicons-admin-generic',
-			);
-			$pages['page2'] = array(
-				'title' => __( 'Page 2' ),
-				'name'  => 'page2',
-				'icon'  => 'dashicons dashicons-admin-generic',
-			);
-			return $pages;
-		}
+		public function options( $builder ) {
+			$container  = WPO\Container::create( 'page1', 'Page 1', 'dashicons dashicons-admin-generic' );
+			$container2 = WPO\Container::create( 'page2', 'Page 2', 'dashicons dashicons-admin-generic' );
+			$container->text( 'textfield', __( 'Textfield' ) );
+			$container2->container( 'section1', __( 'Section 1' ), 'dashicons dashicons-admin-generic' )
+				->text( 'section1_textfield', __( 'Section1 Texfield' ) );
+			$container2->container( 'section2', __( 'Section 2' ), 'dashicons dashicons-admin-generic' )
+				->text( 'section2_textfield', __( 'Section2 Texfield' ) );
 
-		/**
-		 * Add Sections To WPOnion Settings.
-		 *
-		 * @param array $sections
-		 *
-		 * @return array|mixed
-		 */
-		public function add_sections( $sections = array() ) {
-			$sections['page2/section1'] = array(
-				'title' => __( 'Section 1' ),
-				'name'  => 'section1',
-				'icon'  => 'dashicons dashicons-admin-generic',
-			);
-			$sections['page2/section2'] = array(
-				'title' => __( 'Section 2' ),
-				'name'  => 'section2',
-				'icon'  => 'dashicons dashicons-admin-generic',
-			);
-			return $sections;
-		}
-
-		/**
-		 * Add Fields To WPOnion Settings.
-		 *
-		 * @param array $fields
-		 *
-		 * @return array|mixed
-		 */
-		public function add_fields( $fields = array() ) {
-			$fields['page1']          = array(
-				array(
-					'title' => __( 'Textfield' ),
-					'id'    => 'textfield',
-					'type'  => 'text',
-				),
-			);
-			$fields['page2/section1'] = array(
-				array(
-					'title' => __( 'Section1 Textfield' ),
-					'id'    => 'section1_textfield',
-					'type'  => 'textarea',
-				),
-			);
-			$fields['page2/section2'] = array(
-				array(
-					'title' => __( 'Section2 Textfield' ),
-					'id'    => 'section2_textfield',
-					'type'  => 'text',
-				),
-			);
-			return $fields;
+			$builder->container( $container );
+			$builder->container( $container2 );
 		}
 
 	}
